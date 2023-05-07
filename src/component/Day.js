98f1714f5ace6cdd8dadcbfc7f99fec3c2ea12f7
triptotest01 <triptotest01@gmail.com>
2023-05-07 11:11:08 +0900

    router 연결 + Number()  는 url 문자를 숫자로 변환해줌

--- src/component/Day.js
+++ src/component/Day.js
@@ -7,6 +7,7 @@ export default function Day(){
   // const inputDay=3;
   const {inputDay} =useParams();
 
+  // Number 는 문자를 숫자로 변환해줌  (url주소에 들어온 숫자는 문자로인식!)
   const wordList = dummy.words.filter(word => (word.day===Number(inputDay)))
 // wordList : filter함수로 day가3인 요소들을 담은 배열집합 (words배열집합을 filter재가공한 또다른 배열집합)
  return(
